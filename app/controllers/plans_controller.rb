e90bc785aeb012cbd687dd618a1c1373b06c9934
Tyler Hunt <tyhunt04@gmail.com>
2019-04-20 16:20:32 +0000

    fixed html for views/plans

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -9,5 +9,21 @@ class PlanController < ApplicationController
   end
   
   get '/plans/new' do
+    if !logged_in?
+      erb :'/plans/new'
+    else
+      redirect to '/login'
+    end
   end
+  
+  post '/plans' do
+    @plan = Plan.new(:location => params[:location], :party_number => params[:party_number], :shelter_type => params[:shelter_type])
+    if params[:location] == "" || params[:party_number] == "" || params[:shelter_type] == ""
+      redirect to '/plans/new'
+    else
+      @plan.save
+      redirect to '/plans'
+    end
+  end
+  
 end
